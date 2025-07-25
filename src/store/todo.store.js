5f8ebe0184d904517cfb7e17f2e1d4617657a4e8
carloscabani <cacabani@espol.edu.ec>
2025-07-25 17:30:10 -0500

    feat: implement basic todo store logic with filtering, add, toggle, and delete

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -11,6 +11,8 @@ const state = {
         new Todo('Learn Vue.js'),
         new Todo('Learn Vuex'),
         new Todo  ('Learn Vue Router'),
+        new Todo('Learn Vue Composition API'),
+        new Todo('Learn Vue 3'),
     ],  
 
     filter: Filters.All,
@@ -27,42 +29,70 @@ const loadStore = () => {
     throw new Error('loadStore is not implemented');
 }
 
+
+const getTodos = (filter = Filters.All) => {
+    switch (filter){
+        case Filters.All:
+            return [...state.todos];
+
+        case Filters.Completed:
+            return state.todos.filter(todo => todo.done);
+
+        case Filters.Pending:
+            return state.todos.filter(todo => !todo.done);
+        
+        default:
+            throw new Error(`Option ${filter} is not valid`);
+    }
+}
+
+
 /**
  * 
  * @param {String} description 
  */
 
 const addTodo = (description) => {
-    throw new Error('loadStore is not implemented');
+    if (!description) throw new Error('Description is required');
+    state.todos.push(new Todo(description));
 }
 
 
 const toggleTodo = (todoId) => {
-    throw new Error('loadStore is not implemented');
+    state.todos = state.todos.map(todo => {
+        if (todo.id === todoId) {
+            todo.done = !todo.done;
+            return todo;
+        } 
+        
+        return todo;
+    });
 }
 
 
 const deleteTodo = (todoId) => {
-    throw new Error('loadStore is not implemented');
+    state.todos = state.todos.filter(todo => todo.id !== todoId);
 }
 
 
 const deleteCompleted = () => {
-    throw new Error('loadStore is not implemented');
+    state.todos = state.todos.filter(todo => todo.done);
 }
 
 const setFilter = (newFilter = Filters.All) => {
-    throw new Error('loadStore is not implemented');
+    state.filter = newFilter;
 }
 
 const getCurrentFilter = (newFilter = Filters.All) => {
-    throw new Error('loadStore is not implemented');
+    return state.filter;
 }
 
 export default {
+    addTodo,
     deleteCompleted,
     deleteTodo,
     getCurrentFilter,
+    getTodos,
     initStore,
     loadStore,
     setFilter,
