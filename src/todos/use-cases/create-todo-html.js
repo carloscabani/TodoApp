168aff802a1380b5f17fbb5f98cc2b0767e5e31e
carloscabani <cacabani@espol.edu.ec>
2025-08-02 19:46:59 -0500

    refactor: add validations and optimize to-do list rendering

--- src/todos/use-cases/create-todo-html.js
+++ src/todos/use-cases/create-todo-html.js
@@ -8,9 +8,25 @@ export const createHTML = (todo) => {
 
     if (!todo) throw new Error('Todo is required');
 
-    const html = `<h1>${todo.description}</h1>`;
+    const {done, description, id} = todo;
+
+    const html = `
+                <div class="view">
+                    <input class="toggle" type="checkbox" ${ done ? 'checked' : ''}>
+                    <label>${description}</label>
+                    <button class="destroy"></button>
+                </div>
+                <input class="edit" value="Create a TodoMVC template">
+            `;
+
     const liElement = document.createElement('li');
     liElement.innerHTML = html;
+
+    liElement.setAttribute('data-id', id);
+
+    if (todo.done) {
+        liElement.classList.add('completed');
+    }
 
     return liElement;
 }
--- src/todos/use-cases/render-todos.js
+++ src/todos/use-cases/render-todos.js
@@ -1,6 +1,8 @@
 import { Todo } from "../models/todo.model";
 import { createHTML } from "./";
 
+let element; 
+
 /**
  * 
  * @param {String} elementId 
@@ -8,7 +10,13 @@ import { createHTML } from "./";
  */
 export const renderTodos = (elementId, todos = []) => {
 
-    const element  = document.querySelector(elementId);
+    if (!element) 
+        element = document.querySelector(elementId);
+
+    if (!elementId) throw new Error(`Element ${elementId} not found`);
+    
+    element.innerHTML = '';
+    
     todos.forEach(todo => {
         element.append(createHTML(todo));
     });
