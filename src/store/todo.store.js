9fb431763c736df0ce9e2fae29fc8e56ee524001
carloscabani <cacabani@espol.edu.ec>
2025-08-03 11:40:15 -0500

    feat: implement localStorage integration for todos

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -21,12 +21,20 @@ const state = {
 
 
 const initStore = () => {
-    console.log(state);
+    loadStore();
     console.log('Store initialized');
 }
 
 const loadStore = () => {
-    throw new Error('loadStore is not implemented');
+    if(!localStorage.getItem('state')) return;
+    
+    const { todos = [], filter = Filters.All } = JSON.parse(localStorage.getItem('state'));
+    state.todos = todos; 
+    state.filter = filter;
+}
+
+const saveStateToLocalStorage = () => {
+    localStorage.setItem('state', JSON.stringify(state));
 }
 
 
@@ -55,6 +63,7 @@ const getTodos = (filter = Filters.All) => {
 const addTodo = (description) => {
     if (!description) throw new Error('Description is required');
     state.todos.push(new Todo(description));
+    saveStateToLocalStorage();
 }
 
 
@@ -67,20 +76,24 @@ const toggleTodo = (todoId) => {
         
         return todo;
     });
+    saveStateToLocalStorage();
 }
 
 
 const deleteTodo = (todoId) => {
     state.todos = state.todos.filter(todo => todo.id !== todoId);
+    saveStateToLocalStorage();
 }
 
 
 const deleteCompleted = () => {
     state.todos = state.todos.filter(todo => todo.done);
+    saveStateToLocalStorage();
 }
 
 const setFilter = (newFilter = Filters.All) => {
     state.filter = newFilter;
+    saveStateToLocalStorage();
 }
 
 const getCurrentFilter = (newFilter = Filters.All) => {
